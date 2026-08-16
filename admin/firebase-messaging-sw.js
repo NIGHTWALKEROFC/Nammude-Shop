2fc437ed94802de77d612bfd8cbb2d380946a4d4
NIGHTWALKEROFC <saraamuhammad3@gmail.com>
2026-08-16 19:51:57 +0530

    Update firebase-messaging-sw.js

--- admin/firebase-messaging-sw.js
+++ admin/firebase-messaging-sw.js
@@ -7,12 +7,12 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyBgf9mNKgk1wXrd9VDPxWWul8Mr7dA9dQ0",
+  authDomain: "localshop-website.firebaseapp.com",
+  projectId: "localshop-website",
+  storageBucket: "localshop-website.firebasestorage.app",
+  messagingSenderId: "42171379357",
+  appId: "1:42171379357:web:86bc42a7ebb263fe70ae74",
 });
 
 const messaging = firebase.messaging();
